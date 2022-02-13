a5df378d2f8242394a087c40f69194d5237e51f9
SimonYasn <karasekkarasek97@gmail.com>
2022-02-13 16:53:22 +0300

    add vagrantfile

---
+++ kek.sh
@@ -0,0 +1,13 @@
+sudo apt purge -y python2.7-minimal
+
+#python3 --version
+sudo apt-get update
+yes | sudo apt-get install python3-pip
+pip3 install flask
+pip3 install flask_sqlalchemy
+
+
+
+
+
+
---
+++ start.bat
@@ -0,0 +1,3 @@
+pip install flask
+pip install flask_sqlalchemy
+python app.py
